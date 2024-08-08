c5ce91590af3ca07fd9371ee38c812105fb885e9
Blockchainbird <blockchainbird@protonmail.com>
2024-08-08 20:42:18 -0300

    Ex 08 Par_ou_Impar

--- Academy/Exercícios/Lógica/Exercício 08 - Lógica.py
+++ Academy/Exercícios/Lógica/Exercício 08 - Lógica.py
@@ -1,3 +1,32 @@
-print("     Atividades - Lógica - Polkadot Academy     ")
-print("------------------------------------------------")
-print("     Atividade 8 - Par ou Impar com Função      ")
+def cab_logica(tit):
+    print("Atividades - Lógica - Polkadot Academy")
+    print("-" *40)
+    print(tit)
+    print("-" *40)
+
+
+cab_logica('Atividade 8 - Par_ou_Impar Função')
+
+
+def parimpar():
+
+    num = int(input('Digite um número para verificação: '))
+
+    if num % 2 == 0:
+        print(f" -> O número {num} é par!")
+    else:
+        print(f" -> O número {num} é ímpar!")
+    
+
+parimpar()
+
+
+    
+
+
+
+    
+    
+
+
+
