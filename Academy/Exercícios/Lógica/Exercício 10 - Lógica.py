4674cb98a45c5c3dfad90fb12340e1036d0fadc4
Blockchainbird <blockchainbird@protonmail.com>
2024-08-08 21:47:37 -0300

    Inclusão do Ex 10

---
+++ Academy/Exercícios/Lógica/Exercício 10 - Lógica.py
@@ -0,0 +1,8 @@
+def cab_logica(tit):
+    print("Atividades - Lógica - Polkadot Academy")
+    print("-" *45)
+    print(tit)
+    print("-" *45)
+
+
+cab_logica('Atividade 10 - Encontrando numeros primos')
